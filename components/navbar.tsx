6fc5269d30fc26247b23b063ccba37bb2ad6c77e
heistifeh <teefeh26@gmail.com>
2025-09-20 14:46:40 +0100

    project dependencies

---
+++ components/navbar.tsx
@@ -0,0 +1,17 @@
+// 'use client'
+// export default function Navbar() {
+//     return (
+//         <header>
+//             <div>
+//                 Navbar
+//                 <div>
+//                     <Trello />
+//                     <span>Trello Clone</span>
+//                 </div>
+
+
+//             </div>
+//         </header>
+
+//     )
+// }
